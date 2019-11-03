9cd2c3887c0b9c583a8a5a4b401fa76fc44f9d69
Arron M <marshall.arron@gmail.com>
2019-11-03 11:05:14 -0800

    Set up main StateMachine

---
+++ engine/StateMachine.gd
@@ -0,0 +1,47 @@
+extends Node2D
+class_name StateMachine
+
+var controller : Node2D
+
+
+func _init() -> void:
+  pass
+
+
+func _ready() -> void:
+  # disable initial tick processes
+  set_active(false)
+
+  controller = get_parent()
+
+
+func init():
+  # enable tick processes
+  set_active(true)
+
+  # add any other state initialization here
+
+
+func cleanup():
+  # disable tick processes
+  set_active(false)
+
+  # add any other state cleanup here
+
+
+func physics_process(delta : float) -> void:
+  pass
+
+
+func process(delta : float) -> void:
+  pass
+
+
+func input(event : InputEvent) -> void:
+  pass
+
+
+func set_active(boolean):
+  set_physics_process(boolean)
+  set_process_input(boolean)
+  set_process(boolean)
