be1e148fbf58936b09cdcd50009400f71fbf07c4
Arron M <marshall.arron@gmail.com>
2019-11-05 12:16:39 -0800

    Begin moving request logic to global node

--- global.gd
+++ global.gd
@@ -1,3 +1,27 @@
 extends Node
 
 var load_direction : String = ''
+
+func _ready():
+	print('runs')
+	var tree = get_tree().get_root().get_node('global')
+	#print(tree.get_node('global'))
+	var req = HTTPRequest.new()
+	req.connect('request_completed', self, '_request_done')
+	#tree.add_child(req)
+	tree.add_child(req)
+	req.set_owner(tree)
+	
+	req.request('http://www.mocky.io/v2/5185415ba171ea3a00704eed')
+	
+	print(tree.get_node('Room'))
+	#print(tree.get_node('Room'))
+
+func _request_done( result, response_code, headers, body ):
+	print('\ndone done done\n')
+	var json = JSON.parse(body.get_string_from_utf8())
+	print(json.result)
+	#print(json.result.exits)
+	#exits = json.result.exits
+	#terrain = json.result.terrain
+	#request.resume()
--- room/Room.gd
+++ room/Room.gd
@@ -1,11 +1,45 @@
 extends Node2D
+var request
+var exits
+var terrain
 
-#0, 5 > -1, 5 (Left)
-#8, 0 > 8, -1 (Top)
-#8, 10 > 8, 11 (Bottom)
-#16, 5 > 17, 5 (Right)
+onready var tiles = {
+	'CAVE_Floor': $WallMap.tile_set.find_tile_by_name('Caves_Floor'),
+	'CAVE_Wall': $WallMap.tile_set.find_tile_by_name('Caves_Wall'),
+	'Mountain_Floor': $WallMap.tile_set.find_tile_by_name('Mountain_Floor'),
+	'Mountain_Wall': $WallMap.tile_set.find_tile_by_name('Mountain_Wall'),
+	'NORMAL_Floor': $WallMap.tile_set.find_tile_by_name('Plains_Floor'),
+	'NORMAL_Wall': $WallMap.tile_set.find_tile_by_name('Plains_Wall'),
+	'Start-Shop_Floor': $WallMap.tile_set.find_tile_by_name('Start-Shop_Floor'),
+	'Start-Shop_Wall': $WallMap.tile_set.find_tile_by_name('Start-Shop_Wall')
+}
+
+var exit_tiles = {
+	'w': [Vector2(0, 5), Vector2(-1, 5)],
+	'n': [Vector2(8, 0), Vector2(8, -1)],
+	's': [Vector2(8, 10), Vector2(8, 11)],
+	'e': [Vector2(16, 5), Vector2(17, 5)],
+}
 
 # From 1,1 to 15,15 lay down 
+
+func get_data():
+	$HTTPRequest.request("https://lambda-treasure-hunt.herokuapp.com/api/adv/init", ["Authorization: Token ac6e9fac44b48a6974eb8add0a3715184057be52"])
+	yield()
+	for x in range(19):
+		for y in range(13):
+			if (x < 2 or x > 16) or (y < 2 or y > 10):
+				#wall
+				$WallMap.set_cellv(Vector2(x - 1, y - 1), tiles[terrain + '_Wall'])
+			else:
+				#grass
+				$WallMap.set_cellv(Vector2(x - 1, y - 1), tiles[terrain + '_Floor'])
+	
+	for exit in exits:
+		for tile in exit_tiles[exit]:
+			$WallMap.set_cellv(tile, tiles[terrain + '_Floor'])
+
+	$WallMap.update_bitmask_region()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,15 +53,17 @@ func _ready():
 	#print($WallMap.tile_set.find_tile_by_name('Plains_Floor'))
 	#$WallMap.set_cellv(Vector2(-1, -1), 7)
 	
-	$HTTPRequest.request("https://lambda-treasure-hunt.herokuapp.com/api/adv/init", ["Authorization: ac6e9fac44b48a6974eb8add0a3715184057be52"])
+	#get_data()
+	request = get_data()
+	#data.resume()
+	print('resumed')
 
-	for x in range(19):
-		for y in range(13):
-			if (x < 2 or x > 16) or (y < 2 or y > 10):
-				#wall
-				$WallMap.set_cellv(Vector2(x - 1, y - 1), 7)
-			else:
-				#grass
-				$WallMap.set_cellv(Vector2(x - 1, y - 1), 8)
+	
 
-	$WallMap.update_bitmask_region()
+func _on_HTTPRequest_request_completed( result, response_code, headers, body ):
+	var json = JSON.parse(body.get_string_from_utf8())
+	print(json.result)
+	#print(json.result.exits)
+	exits = json.result.exits
+	terrain = json.result.terrain
+	request.resume()
